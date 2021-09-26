050ddf8d3c937c0b1fec422d5cad8b37e8f12c03
Sup3r-Us3r <maydersonmello@gmail.com>
2021-09-26 11:53:19 -0300

    challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { Button } from "./Button";
 
 interface SideBarProps {
@@ -10,11 +11,11 @@ interface SideBarProps {
   buttonClickCallback: (args: any) => void;
 }
 
-export function SideBar({
+const SideBarComponent = ({
   genres,
   selectedGenreId,
   buttonClickCallback
-}: SideBarProps) {
+}: SideBarProps) => {
   return (
     <nav className="sidebar">
       <span>Watch<p>Me</p></span>
@@ -34,3 +35,5 @@ export function SideBar({
     </nav>
   )
 }
+
+export const SideBar = memo(SideBarComponent);
